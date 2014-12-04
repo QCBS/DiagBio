5874e04709b7d93f4bc24a278c796982abefe5df
meganbeneteau <megan_4@live.ca>
2014-12-04 13:28:15 -0500

    changing the spacing of the text to make it fit better (hopefully)

--- php/tabs/tab7.php
+++ php/tabs/tab7.php
@@ -20,22 +20,23 @@ while($count <= $numOfRows)		//creates the selectble classifications
 		if($row['c_name'] == "Matières minérales et métalliques")
 		{
 			echo '<h4>Ressources naturelles non issues du vivant</h4>'; 
-			echo 'Les minéraux, substances nutritionnelles et énergie renouvelable nous sont<br>
-			 	  bénéfiques mais ne sont pas issus du vivant et ne peuvent donc pas être <br>
-			 	  considérés comme des services écosystémiques. Cependant, ils ont un rôle<br>
-			 	  à jouer pour la biodiversité et sont donc à prendre en compte lors de<br>
-			 	  l’évaluation.<br><br>';
+			echo 'Les minéraux, substances nutritionnelles et énergie renouvelable <br> 
+				nous sont bénéfiques mais ne sont pas issus du vivant et ne peuvent <br> 
+				donc pas être considérés comme des services écosystémiques. Cependant,<br> 
+				ils ont un rôle à jouer pour la biodiversité et sont donc à prendre<br> 
+				en compte lors de l’évaluation.<br><br>';
 		}
 
 		if($row['c_name'] == "Énergie renouvelable issue du vivant")
 		{
 			echo '<br><br><h4>Ressources naturelles issues du vivant</h4>';
-			echo 'Les ressources naturelles issues du vivant sont le résultat des interactions<br>';
-			echo 'entre les différents éléments de la biodiversité et donc du vivant. Par<br>'; 
-			echo 'exemple, le pétrole n’est pas en soi un SE, mais plutôt le résultat des<br>';
-			echo 'processus de décomposition, de stockage et d’assimilation, propre aux<br>';
-			echo 'écosystèmes. Son extraction et la consommation de pétrole font pression<br>';
-			echo 'sur le service : régulation de la qualité de l’air. <br><br>';
+			echo 'Les ressources naturelles issues du vivant sont le résultat des<br> 
+			interactions entre les différents éléments de la biodiversité et <br> 
+			donc du vivant. Par exemple, le pétrole n’est pas en soi un SE, mais<br>
+			plutôt le résultat des processus de décomposition, de stockage et<br>
+			d’assimilation, propre aux écosystèmes. Son extraction et la<br>
+			consommation de pétrole font pression sur le service :<br>
+			régulation de la qualité de l’air. <br><br>';
 		}
 ?>
 		<li class="ui-state-default" title="choisissez une de ces classifications" id=<?php echo "\"" . $row['c_id'] . "\""; ?>><?php echo $row['c_name'];?></li>
